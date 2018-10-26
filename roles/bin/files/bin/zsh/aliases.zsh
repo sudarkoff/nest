f4ab2143e5cb50782852fe3dab6ec54173b542f4
George Sudarkoff <georges@surveymonkey.com>
2018-10-26 07:15:10 -0700

    Remove mentiones of DROPBOX, rename llog to wlog (stands for work log), remove some dirmarks I no longer use.

--- roles/bin/files/bin/zsh/aliases.zsh
+++ roles/bin/files/bin/zsh/aliases.zsh
@@ -1,14 +1,4 @@
-alias llog='$HOME/bin/log'
-alias jb='ssh jumpbox.jungle.tech'
-
-if [ "$DROPBOX$" = "$" ]; then
-  export DROPBOX=$HOME/Dropbox
-fi
-
-export DROPBOX_HOME=$DROPBOX/Users/George
+alias wlog='$HOME/bin/log'
 
 m bin ~/bin
 m src ~/src
-m docs $HOME/Documents/writing/documents
-m blog $HOME/Documents/writing/blog
-
